fa27a7bd4ef453df5346dc0929327eef3560da38
guanyou <b08902079@ntu.edu.tw>
2019-12-23 22:28:06 +0800

    arguments.py and main.py Also, the output will be written in output.csv.

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ from args import get_args
 if __name__ == '__main__':
     args = get_args()
     crawler = Crawler()
-    content = crawler.crawl(args.start_date, args.end_date)
+    contents = crawler.crawl(args.start_date, args.end_date)
     # TODO: write content to file according to spec
+    with open(args.output, 'w') as f:
+    	for date, title, content in contents:
+    		title = title.replace('\n', '').replace('\r', '').replace('\"', '\"\"')
+    		content = content.replace('\n', '').replace('\r', '').replace('\"', '\"\"')
+    		output_str = f'{str(date)}, "{title}", "{content}"\n'
+    		f.write(output_str)
